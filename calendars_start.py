11faafbeef75e90144d39afdd53da4fb7a59c3a0
Combatd <combatdstar@gmail.com>
2021-06-05 16:06:25 -0700

    Print out calendars

---
+++ calendars_start.py
@@ -0,0 +1,38 @@
+import calendar
+
+# create a plain text calendar
+c = calendar.TextCalendar(calendar.MONDAY)
+
+# st = c.formatmonth(2017, 1, 0, 0)
+# print(st)
+
+
+# create HTML formatted calendar
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+st = hc.formatmonth(2017, 1)
+print(st)
+
+
+for i in c.itermonthdays(1017, 8):
+    print(i)
+
+for name in calendar.month_name:
+    print(name)
+
+for day in calendar.day_name:
+    print(day)
+
+
+
+print("Team meetings wil be on: ")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(2018, m)
+    weekone=cal[0]
+    weektwo=cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+print("%10s %2d" % (calendar.month_name[m], meetday))
