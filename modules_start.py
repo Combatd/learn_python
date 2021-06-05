07563df37e0a6f637c66593f12b702896f3438d8
Combatd <combatdstar@gmail.com>
2021-06-04 19:38:53 -0700

    WImport Modules

---
+++ modules_start.py
@@ -0,0 +1,9 @@
+# import math module
+import math
+
+#  math module contains lots of pre-build functions
+print("The square root of 16 is", math.sqrt(16))
+
+
+#  some modules contain useful constants
+print("Pi is:", math.pi)
